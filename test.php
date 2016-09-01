983e71ff5074f4aa2493ec597cf73a70cbe2f3cd
990907yang <yam09071004@gmail.com>
2016-09-01 10:47:41 +0900

    조금 추가

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-   phpinfo();
+   //phpinfo();
     //주석임
     /*여러줄 주석임 ㅇㅋ?*/
     //print "출력문임 저는 당신의 똥입니다."; 
@@ -15,7 +15,15 @@
         <ul>
           <li>1</li>
           <li>2</li>
-          <li>3</li>
+          <li>3번째 예제<?php
+              print PHP_VERSION;
+              print PHP_OS;
+              define("HELLO","안녕하시다");
+              print HELLO;
+              print __FILE__;
+              print __DIR__;
+              print "dddasfasdfasdfasdfasdfasdf";
+              ?></li>
           <?php
             print "알겠습니다."
           ?>
