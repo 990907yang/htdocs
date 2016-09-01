a4dd65aa7dab556d548703f5308c50832aaa3cda
990907yang <yam09071004@gmail.com>
2016-09-01 10:26:13 +0900

    삽입은 insert

---
+++ test.php
@@ -0,0 +1,28 @@
+<?php
+   phpinfo();
+    //주석임
+    /*여러줄 주석임 ㅇㅋ?*/
+    //print "출력문임 저는 당신의 똥입니다."; 
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>yang</title>
+</head>
+<body>
+    <header>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <?php
+            print "알겠습니다."
+          ?>
+          <li>4</li>
+      </ul>
+    </header>
+    <section>b</section>
+    <footer>c</footer>
+</body>
+</html>
