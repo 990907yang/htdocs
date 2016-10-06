64ceced50f9bf7fe24b273ed134827b4faa9559b
yang990907 <yam09071004@gmail.com>
2016-10-06 10:24:36 +0900

    전화번호 정규식 추가

--- kcc/signup_check.php
+++ kcc/signup_check.php
@@ -3,8 +3,14 @@
 //    $sql = "INSERT INTO member ('name','year','month','day','id','password','ok','email','address') values ('{$_POST['name']}','{$_POST['year']}','{$_POST['month']}','{$_POST['day']}','{$_POST['id']}','{$_POST['password']}','{$_POST['ok']}','{$_POST['email']}','{$_POST['address']}')";
 
 $pnum = $_POST["phone1"].$_POST["phone2"].$_POST["phone3"];
+
+$phone = preg_replace("/(^02.{0}|^01.{1}|[0-9]{3})([0-9]+)([0-9]{4})/","$1-$2-$3", $pnum);
+
+print $phone;
+
 $address = $_POST["address1"].$_POST["address2"];
-$sql = "insert into member set name='{$_POST["name"]}',year='{$_POST['year']}',month='{$_POST['month']}',day='{$_POST['day']}',id='{$_POST['id']}',password='{$_POST['password']}',pnum='{$pnum}',ok='{$_POST['ok']}',email='{$_POST['email']}',add_num='{$_POST['add_num']}',address='{$address}'";
+$sql = "insert into member set name='{$_POST["name"]}',year='{$_POST['year']}',month='{$_POST['month']}',day='{$_POST['day']}',id='{$_POST['id']}',password='{$_POST['password']}',pnum='{$phone}',ok='{$_POST['ok']}',email='{$_POST['email']}',add_num='{$_POST['add_num']}',address='{$address}'";
+
     $go = $pdo->query($sql);
 
     echo "<form method='post' action='kcc_view.php'>"."출력하기";
