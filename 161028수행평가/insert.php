768d58ecaf2411a45366c395f413afc87d85edee
Pretty_Yang <yam09071004@gmail.com>
2016-11-19 23:27:23 +0900

    Delete insert.php

--- 161028수행평가/insert.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "db.php";
-    $sql = "insert into board set title='{$_POST['title']}',writer='{$_POST['writer']}',content='{$_POST['content']}'";
-    $go = $pdo->query($sql);
-?>
-<script>
-    location.href="sub1.php";
-</script>
