c918c469f6100cc6af49f8760cb0dab9da62893c
Pretty_Yang <yam09071004@gmail.com>
2016-11-19 23:27:05 +0900

    Delete modify.php

--- 161028수행평가/modify.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include "db.php";
-    $sql = "update board set title='{$_POST['title']}',writer='{$_POST['writer']}',content='{$_POST['content']}' where idx='{$_GET['idx']}'";
-    $go = $pdo->query($sql);
-?>
-
-<script>
-    location.href="sub1.php";
-</script>
