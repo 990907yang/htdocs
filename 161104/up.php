8ee00e842ddc6f1e8b6bc39b513e6453c3089755
990907yang <yam09071004@gmail.com>
2016-11-20 03:06:46 +0900

    폴더정리완료

--- 161104/up.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "db.php";
-    $sql = "update board set title='{$_POST['title']}', name='{$_POST['name']}' where idx='{$_GET['idx']}'";
-    $go = $pdo->query($sql);
-?>
-<script>
-    location.href='list.php';
-</script>
