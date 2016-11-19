e1070bef1b29ed70b2281db661e00ca25028eccc
990907yang <yam09071004@gmail.com>
2016-11-20 03:04:06 +0900

    폴더정리

--- ii/del.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "db.php";
-    $sql = "delete from board where idx='{$_GET['idx']}'";
-    $pdo->query($sql);
-?>
-<script>
-    location.href="board.php";
-</script>
--- ii/insert.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "db.php";
-    $sql = "insert into board set writer='{$_POST['writer']}',title='{$_POST['title']}',content='{$_POST['content']}'";
-    $pdo->query($sql);
-?>
-<script>
-    location.href="board.php";
-</script>
