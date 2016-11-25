e774f36500698f80c337d06aaaa7af050018221a
yang990907 <yam09071004@gmail.com>
2016-11-25 11:38:49 +0900

    api 활용해서 만드는 과제 완성

--- bus/busss.php
+++ bus/busss.php
@@ -1,13 +1,13 @@
 <?php
 	$ch = curl_init();
-	//$code=$_GET['code'];//역이름
+	$code=$_GET['code'];//역이름
 	$url = 'http://swopenAPI.seoul.go.kr/api/subway'; /*URL*/
-	$queryParams = '/'. '6d7852724779616d313137664b526c55';
-	$queryParams .= '/'. urlencode('json');
+	$queryParams = '/'. urlencode('6d7852724779616d313137664b526c55');
+	$queryParams .= '/'. urlencode('xml');
 	$queryParams .= '/'. urlencode("firstLastTimetable");
 	$queryParams .= '/'. urlencode('1');
 	$queryParams .= '/'. urlencode('5');
-	$queryParams .= '/'. urlencode("미아");
+	$queryParams .= '/'. urlencode($code);
 
 	curl_setopt($ch, CURLOPT_URL,$url.$queryParams);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
@@ -18,6 +18,4 @@
 	$response = curl_exec($ch);
 	
 	curl_close($ch);
-
-
 ?>
